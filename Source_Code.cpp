3616b11bd22eaa49b985291a0bab93db1d647df0
NitoBoritto <184853774+NitoBoritto@users.noreply.github.com>
2025-11-24 17:53:10 +0200

    Added a node close

--- Source_Code.cpp
+++ Source_Code.cpp
@@ -1,6 +1,12 @@
+// Don't forget to Use comments for every change or new line of code gays
 #include <iostream>
 using namespace std;
 
+class Node {
+public:
+
+};
+
 int main(void) {
-    cout << "Hello World";
+
 }
